ffd7cc71b12be1a84aa15c4efe38eae554501a5a
Steffen Schröder <schteffens@gmail.com>
2013-11-28 07:29:22 +0100

    - Menu ScriptFeature - Pages ScriptFeature - fixed tooltip class + example - menu example - pages example - quad sub classes - improved main script

---
+++ examples/menu.php
@@ -0,0 +1,65 @@
+<?php
+use FML\Controls\Quads\Quad_Emblems;
+
+// Include FML
+require_once __DIR__ . '/../FML/autoload.php';
+
+// Create manialink
+$manialink = new FML\ManiaLink();
+
+// Create menu item list
+$menuItem0 = new Quad_Emblems();
+$manialink->add($menuItem0);
+$menuItem0->setPosition(-50, 30);
+$menuItem0->setSize(20, 20);
+$menuItem0->setSubStyle(Quad_Emblems::SUBSTYLE_0);
+
+$menuItem1 = new Quad_Emblems();
+$manialink->add($menuItem1);
+$menuItem1->setPosition(-50, 0);
+$menuItem1->setSize(20, 20);
+$menuItem1->setSubStyle(Quad_Emblems::SUBSTYLE_1);
+
+$menuItem2 = new Quad_Emblems();
+$manialink->add($menuItem2);
+$menuItem2->setPosition(-50, -30);
+$menuItem2->setSize(20, 20);
+$menuItem2->setSubStyle(Quad_Emblems::SUBSTYLE_2);
+
+// Create submenus
+$subMenu0 = new Quad_Emblems();
+$manialink->add($subMenu0);
+$subMenu0->setPosition(10, 0);
+$subMenu0->setSize(50, 50);
+$subMenu0->setSubStyle(Quad_Emblems::SUBSTYLE_0);
+
+$subMenu1 = new Quad_Emblems();
+$manialink->add($subMenu1);
+$subMenu1->setPosition(10, 0);
+$subMenu1->setSize(50, 50);
+$subMenu1->setSubStyle(Quad_Emblems::SUBSTYLE_1);
+
+$subMenu2 = new Quad_Emblems();
+$manialink->add($subMenu2);
+$subMenu2->setPosition(10, 0);
+$subMenu2->setSize(50, 50);
+$subMenu2->setSubStyle(Quad_Emblems::SUBSTYLE_2);
+
+// Create script
+$script = new FML\Script\Script();
+$manialink->setScript($script);
+
+// Create menu
+$menus = new FML\Script\Menus();
+$script->addFeature($menus);
+
+$menuRelationships = array();
+array_push($menuRelationships, array($menuItem0, $subMenu0));
+array_push($menuRelationships, array($menuItem1, $subMenu1));
+array_push($menuRelationships, array($menuItem2, $subMenu2));
+$menus->add($menuRelationships);
+
+// Print xml
+$manialink->render(true);
+
+?>
--- examples/tooltip.php
+++ examples/tooltip.php
@@ -7,12 +7,17 @@ require_once __DIR__ . '/../FML/autoload.php';
 $manialink = new FML\ManiaLink();
 
 // Create quad for which a tooltip will be shown
-$quad = new FML\Controls\Quad();
+$quad = new FML\Controls\Quads\Quad_Emblems();
 $manialink->add($quad);
+$quad->setSize(50, 50);
+$quad->setSubStyle($quad::SUBSTYLE_1);
 
 // Create the tooltip quad
-$tooltipQuad = new FML\Controls\Quad();
+$tooltipQuad = new FML\Controls\Quads\Quad_Emblems();
 $manialink->add($tooltipQuad);
+$tooltipQuad->setPosition(50, 50);
+$tooltipQuad->setSize(50, 50);
+$tooltipQuad->setSubStyle($tooltipQuad::SUBSTYLE_2);
 
 // Create script
 $script = new FML\Script\Script();
