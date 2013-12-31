121867ca9aece4c7fe81cceb420d81b9504dbdee
Steffen Schröder <mail@steeffeen.com>
2013-12-31 02:53:00 +0100

    Removed not-used method

--- FML/Script/Builder.php
+++ FML/Script/Builder.php
@@ -10,40 +10,10 @@ namespace FML\Script;
 abstract class Builder {
 
 	/**
-	 * Build a ManiaScript Texts Array with the given Elements
-	 *
-	 * @param array $elements        	
-	 * @param bool $associative        	
-	 * @return string
-	 */
-	public static function getTextsArray(array $elements, $associative = false) {
-		$arrayText = '[';
-		$elementsCount = count($elements);
-		if ($elementsCount <= 0) {
-			$arrayText .= '""';
-		}
-		else {
-			$i = 1;
-			foreach ($elements as $key => $value) {
-				if ($associative) {
-					$arrayText .= '"' . $key . '"=>';
-				}
-				$arrayText .= '"' . $value . '"';
-				if ($i < $elementsCount) {
-					$arrayText .= ',';
-					$i++;
-				}
-			}
-		}
-		$arrayText .= ']';
-		return $arrayText;
-	}
-
-	/**
 	 * Build a Label Implementation Block
 	 *
-	 * @param string $labelName        	
-	 * @param string $implementationCode        	
+	 * @param string $labelName
+	 * @param string $implementationCode
 	 * @return string
 	 */
 	public static function getLabelImplementationBlock($labelName, $implementationCode) {
