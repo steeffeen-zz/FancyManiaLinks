d8a4230ac973c849e89696582472f6802d8fa56c
Steffen Schröder <schteffens@gmail.com>
2014-01-02 13:21:14 +0100

    cloning example

---
+++ examples/cloning.php
@@ -0,0 +1,28 @@
+<?php
+
+// Include FML
+require_once __DIR__ . '/../FML/autoload.php';
+
+// Create manialink
+$manialink = new FML\ManiaLink();
+
+// Create content frame
+$frame = new FML\Controls\Frame();
+$manialink->add($frame);
+
+// Create first label with various styling attributes
+$firstLabel = new FML\Controls\Labels\Label_Text();
+$frame->add($firstLabel);
+$firstLabel->setX(-20);
+$firstLabel->setStyle($firstLabel::STYLE_TextTitle1);
+$firstLabel->setTextSize(3);
+$firstLabel->setText('Label 1');
+
+// Create cloned second label and adjust only the position and text
+$secondLabel = clone $firstLabel;
+$frame->add($secondLabel);
+$secondLabel->setX(20);
+$secondLabel->setText('Label 2');
+
+// Print xml
+$manialink->render(true);
