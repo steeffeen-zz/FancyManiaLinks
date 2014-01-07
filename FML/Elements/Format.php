2434102b4a2b90e05b05f762a80b8870c5e50e2b
Steffen Schröder <schteffens@gmail.com>
2014-01-08 00:02:08 +0100

    format element

--- FML/Elements/Format.php
+++ FML/Elements/Format.php
@@ -3,22 +3,107 @@
 namespace FML\Elements;
 
 /**
- * Class representing a format
+ * Format Element
  *
  * @author steeffeen
  */
+// TODO: example
 class Format implements BgColorable, Renderable, Styleable, TextFormatable {
 	/**
 	 * Protected Properties
 	 */
 	protected $tagName = 'format';
+	protected $bgColor = '';
+	protected $style = '';
+	protected $textSize = -1;
+	protected $textColor = '';
+	protected $areaColor = '';
+	protected $areaFocusColor = '';
+
+	/**
+	 *
+	 * @see \FML\Types\BgColorable::setBgColor()
+	 * @return \FML\Elements\Format
+	 */
+	public function setBgColor($bgColor) {
+		$this->bgColor = (string) $bgColor;
+		return $this;
+	}
+
+	/**
+	 *
+	 * @see \FML\Types\Styleable::setStyle()
+	 * @return \FML\Elements\Format
+	 */
+	public function setStyle($style) {
+		$this->style = (string) $style;
+		return $this;
+	}
+
+	/**
+	 *
+	 * @see \FML\Types\TextFormatable::setTextSize()
+	 * @return \FML\Elements\Format
+	 */
+	public function setTextSize($textSize) {
+		$this->textSize = (int) $textSize;
+		return $this;
+	}
+
+	/**
+	 *
+	 * @see \FML\Types\TextFormatable::setTextColor()
+	 * @return \FML\Elements\Format
+	 */
+	public function setTextColor($textColor) {
+		$this->textColor = (string) $textColor;
+		return $this;
+	}
+
+	/**
+	 *
+	 * @see \FML\Types\TextFormatable::setAreaColor()
+	 * @return \FML\Elements\Format
+	 */
+	public function setAreaColor($areaColor) {
+		$this->areaColor = (string) $areaColor;
+		return $this;
+	}
+
+	/**
+	 *
+	 * @see \FML\Types\TextFormatable::setAreaFocusColor()
+	 * @return \FML\Elements\Format
+	 */
+	public function setAreaFocusColor($areaFocusColor) {
+		$this->areaFocusColor = (string) $areaFocusColor;
+		return $this;
+	}
 
 	/**
 	 *
 	 * @see \FML\Renderable::render()
 	 */
 	public function render(\DOMDocument $domDocument) {
-		$xml = $domDocument->createElement($this->tagName);
-		return $xml;
+		$xmlElement = $domDocument->createElement($this->tagName);
+		if ($this->bgColor) {
+			$xmlElement->setAttribute('bgcolor', $this->bgColor);
+		}
+		if ($this->style) {
+			$xmlElement->setAttribute('style', $this->style);
+		}
+		if ($this->textSize >= 0) {
+			$xmlElement->setAttribute('textsize', $this->textSize);
+		}
+		if ($this->textColor) {
+			$xmlElement->setAttribute('textcolor', $this->textColor);
+		}
+		if ($this->areaColor) {
+			$xmlElement->setAttribute('areacolor', $this->areaColor);
+		}
+		if ($this->areaFocusColor) {
+			$xmlElement->setAttribute('areafocuscolor', $this->areaFocusColor);
+		}
+		return $xmlElement;
 	}
 }
