90d6bdb3770cc60d5aead5b5d7a4b3a07cad7add
Steffen Schröder <schteffens@gmail.com>
2014-01-19 19:26:18 +0100

    changed container to control class children

--- FML/Types/Container.php
+++ FML/Types/Container.php
@@ -3,6 +3,7 @@
 namespace FML\Types;
 
 use FML\Controls\Control;
+use FML\Elements\Format;
 
 /**
  * Interface for Element being able to contain other Controls
@@ -15,11 +16,30 @@ interface Container {
 	 * Add a new Child Control
 	 *
 	 * @param Control $child The Child Control to add
+	 * @return \FML\Types\Container
 	 */
 	public function add(Control $child);
 
 	/**
 	 * Remove all Children
+	 *
+	 * @return \FML\Types\Container
 	 */
 	public function removeChildren();
+
+	/**
+	 * Set the Format Object of the Container
+	 *
+	 * @param Format $format New Format Object
+	 * @return \FML\Types\Container
+	 */
+	public function setFormat(Format $format);
+
+	/**
+	 * Get the Format Object of the Container
+	 * 
+	 * @param bool $createIfEmpty (optional) Whether the Format Object should be created if it's not set yet
+	 * @return \FML\Elements\Format
+	 */
+	public function getFormat($createIfEmpty = true);
 }
