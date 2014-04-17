6f9dbf5f74113b7d2fcf78a0bf18636351ea30fd
Steffen Schröder <schteffens@gmail.com>
2014-04-17 18:52:54 +0200

    classes representing maniascript includes, constants & labels

---
+++ FML/Script/ScriptInclude.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace FML\Script;
+
+/**
+ * Class representing an Include of the ManiaLink Script
+ *
+ * @author steeffeen
+ * @copyright FancyManiaLinks Copyright © 2014 Steffen Schröder
+ * @license http://www.gnu.org/licenses/ GNU General Public License, Version 3
+ */
+class ScriptInclude {
+	/*
+	 * Constants
+	 */
+	const MATHLIB = 'MathLib';
+	const TEXTLIB = 'TextLib';
+	
+	/*
+	 * Public Properties
+	 */
+	public $file = null;
+	public $namespace = null;
+
+	/**
+	 * Construct a new Script Include
+	 * 
+	 * @param string $file (optional) Include File
+	 * @param string $namespace (optional) Include Namespace
+	 */
+	public function __construct($file = null, $namespace = null) {
+		$this->setFile($file);
+		$this->setNamespace($namespace);
+	}
+
+	/**
+	 * Set the File
+	 *
+	 * @param string $file Include File
+	 * @return \FML\Script\ScriptInclude
+	 */
+	public function setFile($file) {
+		$this->file = $file;
+		return $this;
+	}
+
+	/**
+	 * Set the Namespace
+	 *
+	 * @param string $text Include Namespace
+	 * @return \FML\Script\ScriptInclude
+	 */
+	public function setNamespace($namespace) {
+		$this->namespace = $namespace;
+		return $this;
+	}
+	
+	/**
+	 * Build the Script Include Text
+	 * 
+	 * @return string
+	 */
+	public function __toString() {
+		$scriptText = Builder::getInclude($this->file, $this->namespace);
+		return $scriptText;
+	}
+}
---
+++ FML/Script/ScriptLabel.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace FML\Script;
+
+/**
+ * Class representing a Part of the ManiaLink Script
+ *
+ * @author steeffeen
+ * @copyright FancyManiaLinks Copyright © 2014 Steffen Schröder
+ * @license http://www.gnu.org/licenses/ GNU General Public License, Version 3
+ */
+class ScriptLabel {
+	/*
+	 * Constants
+	 */
+	const ONINIT = 'FML_OnInit';
+	const LOOP = 'FML_Loop';
+	const TICK = 'FML_Tick';
+	const ENTRYSUBMIT = 'FML_EntrySubmit';
+	const KEYPRESS = 'FML_KeyPress';
+	const MOUSECLICK = 'FML_MouseClick';
+	const MOUSEOUT = 'FML_MouseOut';
+	const MOUSEOVER = 'FML_MouseOver';
+	
+	/*
+	 * Public Properties
+	 */
+	public $name = null;
+	public $text = null;
+
+	/**
+	 * Construct a new ScriptLabel
+	 * 
+	 * @param string $name (optional) Label Name
+	 * @param string $text (optional) Script Text
+	 */
+	public function __construct($name = self::LOOP, $text = '') {
+		$this->name = $name;
+		$this->text = $text;
+	}
+
+	/**
+	 * Set the Name
+	 *
+	 * @param string $name Label Name
+	 * @return \FML\Script\ScriptLabel
+	 */
+	public function setName($name) {
+		$this->name = $name;
+		return $this;
+	}
+
+	/**
+	 * Set the Text
+	 *
+	 * @param string $text Script Text
+	 * @return \FML\Script\ScriptLabel
+	 */
+	public function setText($text) {
+		$this->text = $text;
+		return $this;
+	}
+	
+	/**
+	 * Build the full Script Label Text
+	 * 
+	 * @return string
+	 */
+	public function __toString() {
+		$scriptText = Builder::getLabelImplementationBlock($this->name, $this->text);
+		return $scriptText;
+	}
+}
