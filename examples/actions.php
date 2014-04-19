27e1c3636597d9a84d51bb29500d959c565b8b47
Steffen Schröder <schteffens@gmail.com>
2014-04-19 12:22:19 +0200

    updated example

--- examples/actions.php
+++ examples/actions.php
@@ -6,19 +6,19 @@ require_once __DIR__ . '/../FML/autoload.php';
 // Create manialink
 $maniaLink = new \FML\ManiaLink();
 
-// Static action
+// Static action via xml
 $homeLabel = new \FML\Controls\Label();
 $maniaLink->add($homeLabel);
 $homeLabel->setX(-10);
 $homeLabel->setText('Home');
 $homeLabel->setAction(\FML\Types\Actionable::ACTION_HOME);
 
-// Dynamic action triggering
+// Dynamic action via script
 $enterLabel = new \FML\Controls\Label();
 $maniaLink->add($enterLabel);
 $enterLabel->setX(10);
 $enterLabel->setText('Enter');
-$maniaLink->getScript()->addPageActionTrigger($enterLabel, \FML\Types\Actionable::ACTION_ENTER);
+$enterLabel->addActionTrigger(\FML\Types\Actionable::ACTION_ENTER);
 
 // Print xml
 $maniaLink->render(true);
