9b365669adf55e1eecf735cecc94a550f5760d7a
Steffen Schröder <schteffens@gmail.com>
2014-05-16 22:48:30 +0200

    applied new code styling

--- examples/simple_classes.php
+++ examples/simple_classes.php
@@ -5,12 +5,10 @@ define('FML_SIMPLE_CLASSES', true);
 
 require_once __DIR__ . '/../FML/autoload.php';
 
-
 // You can use the class names directly without the namespace if they aren't used already
 
 // Instead of: $maniaLink = new FML\ManiaLink();
 $maniaLink = new ManiaLink();
-
 
 // You can also use the simple namespace FML\ without having to give the full namespace
 
@@ -18,7 +16,6 @@ $maniaLink = new ManiaLink();
 $label = new FML\Label_Text();
 $maniaLink->add($label);
 $label->setText('Easy Label.');
-
 
 // Print xml
 $maniaLink->render(true);
