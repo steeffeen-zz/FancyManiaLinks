b90419bb60b76500a6b6ee01485d6bdc0c46eb8c
Steffen Schröder <schteffens@gmail.com>
2014-05-18 23:40:48 +0200

    completed value picker example

--- examples/valuepicker.php
+++ examples/valuepicker.php
@@ -9,6 +9,7 @@ $maniaLink = new \FML\ManiaLink();
 // Create value picker
 $valuePicker = new \FML\Components\ValuePicker('TestValuePicker');
 $maniaLink->add($valuePicker);
+$valuePicker->setValues(array('Hydrogen', 'Helium', 'Lithium', 'Beryllium', 'Boron'));
 
 // Print xml
 $maniaLink->render(true);
