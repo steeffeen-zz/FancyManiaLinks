c0f0c177bb30b45c19d0cfa1106ce31d449b8e26
Steffen Schröder <schteffens@gmail.com>
2014-05-20 10:28:45 +0200

    improved @author annotation

--- FML/Script/Features/ValuePickerFeature.php
+++ FML/Script/Features/ValuePickerFeature.php
@@ -12,7 +12,7 @@ use FML\Script\ScriptLabel;
 /**
  * Script Feature for creating a ValuePicker Behavior
  *
- * @author    steeffeen
+ * @author    steeffeen <mail@steeffeen.com>
  * @copyright FancyManiaLinks Copyright © 2014 Steffen Schröder
  * @license   http://www.gnu.org/licenses/ GNU General Public License, Version 3
  */
