45a20cf1de95c73add73ba29f1375141f38c4df0
Steffen Schröder <schteffens@gmail.com>
2014-06-16 15:58:12 +0200

    removed simple class technology (too dirty)

--- FML/autoload.php
+++ FML/autoload.php
@@ -15,7 +15,6 @@ if (!defined('FML_PATH')) {
 if (!defined('FML_VERSION')) {
 	define('FML_VERSION', '1.2');
 }
-//define('FML_SIMPLE_CLASSES', true);
 
 /*
  * Autoload Function that loads FML Class Files on Demand
@@ -26,57 +25,7 @@ if (!defined('FML_AUTOLOAD_DEFINED')) {
 		$classPath = str_replace('\\', DIRECTORY_SEPARATOR, $className);
 		$filePath  = FML_PATH . $classPath . '.php';
 		if (file_exists($filePath)) {
-			// Load with FML namespace
 			require_once $filePath;
-		} else if (defined('FML_SIMPLE_CLASSES') && FML_SIMPLE_CLASSES) {
-			// Load as simple class name
-			if (!function_exists('loadSimpleClass')) {
-
-				/**
-				 * Load FML Class Files from the given Directory
-				 *
-				 * @param string $className     Class to load
-				 * @param string $directory     Directory to open
-				 * @param array  $excludes      File Names to ignore
-				 * @param string $baseNamespace Base Namespace
-				 * @return bool
-				 */
-				function loadSimpleClass($className, $directory, $excludes, $baseNamespace) {
-					if ($dirHandle = opendir($directory)) {
-						$classParts      = explode('\\', $className);
-						$simpleClassName = end($classParts);
-						$classFileName   = $simpleClassName . '.php';
-						while ($fileName = readdir($dirHandle)) {
-							if (in_array($fileName, $excludes)) {
-								continue;
-							}
-							$filePath = $directory . $fileName;
-							if (is_dir($filePath)) {
-								$subDirectory = $filePath . DIRECTORY_SEPARATOR;
-								$namespace    = $baseNamespace . $fileName . '\\';
-								$success      = loadSimpleClass($className, $subDirectory, $excludes, $namespace);
-								if ($success) {
-									return true;
-								}
-								continue;
-							}
-							if (is_file($filePath)) {
-								if ($fileName == $classFileName) {
-									require_once $filePath;
-									class_alias($baseNamespace . $simpleClassName, $className, false);
-									return true;
-								}
-								continue;
-							}
-						}
-						closedir($dirHandle);
-					}
-					return false;
-				}
-			}
-			$excludes      = array('.', '..');
-			$baseNamespace = 'FML\\';
-			loadSimpleClass($className, FML_PATH . 'FML' . DIRECTORY_SEPARATOR, $excludes, $baseNamespace);
 		}
 	});
 }
