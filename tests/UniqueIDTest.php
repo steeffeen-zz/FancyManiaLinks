66e0d60104d97af012dccb697ddd4dc39f25dc62
Steffen Schröder <schteffens@gmail.com>
2016-06-06 01:55:01 +0200

    added unit test for UniqueID

---
+++ tests/UniqueIDTest.php
@@ -0,0 +1,17 @@
+<?php
+
+require_once __DIR__ . '/../autoload.php';
+
+class UniqueIDTest extends PHPUnit_Framework_TestCase {
+
+	public function testUniqueIDs() {
+		$firstUniqueID  = new \FML\UniqueID();
+		$secondUniqueID = new \FML\UniqueID();
+		$thirdUniqueID = new \FML\UniqueID();
+
+		$this->assertEquals($firstUniqueID->getValue(), "FML_ID_1");
+		$this->assertEquals($secondUniqueID->getValue(), "FML_ID_2");
+		$this->assertEquals($thirdUniqueID->getValue(), "FML_ID_3");
+	}
+
+}
