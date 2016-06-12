983332c3f41fd582c07aeb49dfcfaa4c530cb3c8
Steffen Schröder <schteffens@gmail.com>
2016-06-12 18:28:21 +0200

    added interfaces for Identifiable & Imageable

---
+++ FML/Types/Identifiable.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace FML\Types;
+
+/**
+ * Interface for identifiable Elements
+ *
+ * @author    steeffeen <mail@steeffeen.com>
+ * @copyright FancyManiaLinks Copyright © 2014 Steffen Schröder
+ * @license   http://www.gnu.org/licenses/ GNU General Public License, Version 3
+ */
+interface Identifiable
+{
+
+    /**
+     * Get the Id
+     *
+     * @api
+     * @return string
+     */
+    public function getId();
+
+    /**
+     * Set the Id
+     *
+     * @api
+     * @param string $id ID
+     * @return static
+     */
+    public function setId($id);
+
+}
