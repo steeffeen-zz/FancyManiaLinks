f9f7f1833367f7eac8b452d529899d69ff6fa77d
Steffen Schröder <schteffens@gmail.com>
2017-05-14 13:48:53 +0200

    Improve Collecting of Script Features to prevent Duplicates

--- tests/Script/Features/ScriptFeatureTest.php
+++ tests/Script/Features/ScriptFeatureTest.php
@@ -11,13 +11,24 @@ class ScriptFeatureTest extends \PHPUnit_Framework_TestCase
     {
         $toggle1 = new Toggle();
         $toggle2 = new Toggle();
+        $toggle3 = new Toggle();
         $label   = new Label();
         $label->addScriptFeature($toggle2);
 
         $this->assertEmpty(ScriptFeature::collect(null));
         $this->assertEquals(array($toggle1), ScriptFeature::collect($toggle1));
         $this->assertEquals(array($toggle2), ScriptFeature::collect($label));
-        $this->assertEquals(array($toggle1, $toggle2), ScriptFeature::collect($toggle1, $label));
+        $this->assertEquals(array($toggle1, $toggle2, $toggle3), ScriptFeature::collect($toggle1, $label, array($toggle1, $toggle2, $toggle3)));
+    }
+
+    public function testAddScriptFeature()
+    {
+        $toggle1 = new Toggle();
+        $toggle2 = new Toggle();
+
+        $this->assertEquals(array($toggle1, $toggle2), ScriptFeature::addScriptFeature(array($toggle1), $toggle2));
+        $this->assertEquals(array($toggle1, $toggle2), ScriptFeature::addScriptFeature(array($toggle1, $toggle2), $toggle2));
+        $this->assertEquals(array($toggle1, $toggle2), ScriptFeature::addScriptFeature(array($toggle1), array($toggle1, $toggle2)));
     }
 
 }
